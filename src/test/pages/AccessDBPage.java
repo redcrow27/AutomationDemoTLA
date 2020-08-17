c19d5951f39e22642d45d2848e9a79ac58621d8d
redcrow27 <erdimutlu07@gmail.com>
2020-08-16 23:14:16 -0400

    created AccessDBPageTest and rename: UserDBPage >> to >> AccessDBPage

--- src/test/pages/AccessDBPage.java
+++ src/test/pages/AccessDBPage.java
@@ -4,10 +4,10 @@ import base.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-public class UserDBPage extends BasePage {
+public class AccessDBPage extends BasePage {
     WebDriver driver;
 
-    public UserDBPage(WebDriver driver) {
+    public AccessDBPage(WebDriver driver) {
         super(driver);
         this.driver = driver;
         PageFactory.initElements(driver, this);
---
+++ src/test/tests/AccessDBPageTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.AccessDBPage;
+import pages.CommonPage;
+import pages.LogInPage;
+import pojos.User;
+
+import java.lang.reflect.Method;
+
+public class AccessDBPageTest extends BaseTest {
+    AccessDBPage accessDBPage;
+    CommonPage commonPage;
+    User user;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(Method method, ITestResult result) {
+        super.setUp(method, result);
+        accessDBPage = new AccessDBPage(getDriver());
+        commonPage = new CommonPage(getDriver());
+        commonPage.clickNavBtn("User-Mgt");
+        commonPage.click(commonPage.accessDb_Btn);
+        user = new User();
+    }
+
+    @Test
+    public void validateEnterPage() {
+
+    }
+
+
+    @AfterMethod
+    public void tearDown(){
+        getDriver().quit();
+    }
+
+}
